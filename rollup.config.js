fe658f18af3465fa656915952b3ad482bf3e65d7
budiadiono <budiadiono@gmail.com>
2016-10-10 05:26:16 +0700

    switch from gulp to rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from 'rollup-plugin-typescript';
+var pkg = require('./package.json');
+
+var banner = `/**
+  * ${pkg.name} ${pkg.version}
+  * ${pkg.description}
+  * ${pkg.homepage}
+  
+  * Copyright 2016, ${pkg.author}
+  * Released under the ${pkg.license} license.
+  '*/`;
+
+export default {
+  entry: 'src/index.ts',
+  format: 'amd',
+  dest: 'index.js',
+	moduleName: pkg.name,
+	plugins: [
+    typescript()
+  ],
+	banner:banner
+};
